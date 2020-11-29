32a328acc2a2603b8a0a1fb01d7a404e448df813
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2020-11-29 20:22:59 +0530

    Geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,28 @@
-io()
+const socket = io()
+
+
+socket.on('message', (message) => {
+  console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const message = e.target.elements.message.value
+  socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', (position) => {
+  console.log(position)
+  
+})
+
+// socket.on('countUpdate', (count) => {
+//   console.log('The count has been updated!', count)
+// })
+
+
+// //Sending data from user to server
+// document.querySelector('#increment').addEventListener('click', () => {
+//   console.log('Clicked!')
+//   socket.emit('increment')
+// })
--- src/index.js
+++ src/index.js
@@ -12,9 +12,29 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
+// let count = 0
 
-io.on('connection', () => {
+
+io.on('connection', (socket) => {
   console.log('New Websocket connection')
+  
+  socket.emit('message', 'Welcome!')
+  socket.broadcast.emit('message', 'A new user has joined!') //Send to everyone except current socket
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message)
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!')
+  })
+
+  // socket.emit('countUpdate', count)
+  // socket.on('increment', () => {
+  //   count++
+  //   // socket.emit('countUpdate', count) //Emit only in one window
+  //   io.emit('countUpdate', count)  //Emit to every single connection
+  // })
 })
 
 
@@ -22,3 +42,5 @@ server.listen(port, () => {
   console.log(`Server is up on port ${port}!`)
 })
 
+//Server (emit) -> client (receive) - countUpdate
+//client (emit) -> server (receive) - increment
