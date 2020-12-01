9d30d90ee7bfe1b14fb610416716db0d0fbf8ffb
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2020-12-01 18:04:18 +0530

    init

--- src/index.js
+++ src/index.js
@@ -30,8 +30,13 @@ io.on('connection', (socket) => {
 
     socket.join(user.room)
 
-    socket.emit('message', generateMessage('Welcome!') )
-    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`)) //Send to everyone except current socket
+    socket.emit('message', generateMessage('Admin', 'Welcome!') )
+    socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`)) //Send to everyone except current socket
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUserInRoom(user.room)
+    })
 
     callback()
 
@@ -45,17 +50,19 @@ io.on('connection', (socket) => {
 
 
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
     const filter = new Filter()
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!')
     }
-    io.emit('message', generateMessage(message))
+    io.to(user.room).emit('message', generateMessage(user.username, message))
     callback()
   })
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+    const user = getUser(socket.id)
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
     callback()
   })
 
@@ -63,7 +70,11 @@ io.on('connection', (socket) => {
     const user = removeUser(socket.id)
 
     if (user) {
-      io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+      io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUserInRoom(user.room)
+      })
     }
   })
 
